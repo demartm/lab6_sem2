46a71ff5e427fa14291dfd3fde2408ca9cfb8da5
demartm <artemis20242511@gmail.com>
2026-05-24 18:43:31 -0400

    calculate works

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 struct point {
 
-  char x;
+  int x;
   struct point* next;
 
 };
@@ -17,7 +17,7 @@ struct stack {
 
 };
 
-void pushToStack(struct stack* st,char x){
+void pushToStack(struct stack* st,int x){
 if(st){
 
   struct point* el = (struct point*)malloc(sizeof(struct point));
@@ -33,12 +33,12 @@ if(st){
   return;
 }
 
-char popFromStack(struct stack* st/*,int *code*/){
+int popFromStack(struct stack* st/*,int *code*/){
 if(st){
 
   if(/*st->size > 0 && */st->next){
 
-    char res = st->next->x;
+    int res = st->next->x;
     struct point* next = st->next->next;
 
     free(st->next);
@@ -83,7 +83,7 @@ void printStack(struct stack *st){
 if(st){
   struct point *cur = NULL;
     for(cur = st->next; cur; cur = cur->next){
-      printf("%c\n",cur->x);
+      printf("%d\n",cur->x);
     }
   }
 }
@@ -253,6 +253,89 @@ if(result){
 return NULL;
 }
 
+int calculate(char *expression){
+if(expression){
+
+  struct stack* st = (struct stack*)calloc(sizeof(struct stack),1);
+
+  if(st){
+
+  unsigned char Tab[256] = {0};
+  unsigned char numbers[10] = {'0','1','2','3','4','5','6','7','8','9'};
+  unsigned char low_prior[3] = {'+','-'};
+  unsigned char high_prior[3] = {'/','*'};
+  unsigned char open = '(';
+  unsigned char close = ')';
+  unsigned char space = ' ';
+
+
+  size_t sizeNums = sizeof(numbers);
+  size_t sizeLow = sizeof(low_prior);
+  size_t sizeHigh = sizeof(high_prior);
+
+  for(int i = 0; i < sizeNums; i++){
+    Tab[numbers[i]] = 1;
+  }
+
+  for(int i = 0; i < sizeLow; i++){
+    Tab[low_prior[i]] = 2;
+  }
+
+  for(int i = 0; i < sizeHigh; i++){
+    Tab[high_prior[i]] = 3;
+  }
+  Tab[open] = 4;
+  Tab[close] = 5;
+  Tab[space] = 6;
+
+
+
+size_t ex_size = strlen(expression);
+int temp1 = 0;
+int temp2 = 0;
+for(size_t i = 0; i < ex_size; i++){
+
+  if(Tab[expression[i]] == 1){
+    pushToStack(st,(int)expression[i]-48);
+  }else{
+    if(st->size){
+        temp2 = popFromStack(st);
+        temp1 = popFromStack(st);
+      if(expression[i] == '+'){
+        temp1 += temp2;
+      }else{
+        if(expression[i] == '-'){
+          temp1 -= temp2;
+        }else{
+          if(expression[i] == '*'){
+            temp1 *= temp2;
+            }else{
+            if(expression[i] == '/'){
+               if(temp2 != 0){
+                 temp1 /= temp2;
+               }else{
+                 return 0;
+               }
+            }else{
+
+              return 0;
+
+            }
+          }
+        }
+      }
+      pushToStack(st,temp1);
+    }
+  }
+
+}
+
+return temp1;
+}
+}
+return 0;
+}
+
 
 
 int main()
@@ -284,6 +367,7 @@ for(int i = 0; i < sizeof(expression)/50; i++){
   char *arr = convertToRPN(expression[i],&code);
 if(arr){
 printf("%s\n",arr);
+printf("solve:%d\n\n",calculate(arr));
 }else{
 printf("error %d\n",code);
 }
